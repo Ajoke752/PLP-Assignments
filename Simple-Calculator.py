7bca8ffc945718b5fc29c6a2df098a48839ff4a7
Ajoke752 <ajokeabdulrasaq2@gmail.com>
2025-07-27 14:01:25 +0100

    Simple calculator
    The program does simple calculations like addition, subtraction, multiplication and division

---
+++ Simple-Calculator.py
@@ -0,0 +1,15 @@
+first_number = int(input("please your first number: "))
+
+second_number = int(input("please your first number: "))
+
+# Addition operation
+print(first_number + second_number)
+
+# Subtraction operation
+print(first_number - second_number)
+
+# Multiplication operation
+print(first_number * second_number)
+
+# Division operation
+print(first_number / second_number)
