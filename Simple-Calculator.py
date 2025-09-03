c32d63ec2a4fb059cab07af978a258fd68e7067c
Ajoke752 <ajokeabdulrasaq2@gmail.com>
2025-09-03 01:23:44 -0700

    Made some correction

--- Simple-Calculator.py
+++ Simple-Calculator.py
@@ -1,6 +1,6 @@
 first_number = int(input("please your first number: "))
 
-second_number = int(input("please your first number: "))
+second_number = int(input("please your second number: "))
 
 # Addition operation
 print(first_number + second_number)
@@ -11,5 +11,8 @@ print(first_number - second_number)
 # Multiplication operation
 print(first_number * second_number)
 
-# Division operation
-print(first_number / second_number)
+# Division operation with error handling
+try:
+    print(first_number / second_number)
+except ZeroDivisionError:
+    print("Error: Cannot divide by zero.")
